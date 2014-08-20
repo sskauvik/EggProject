62c08595bffc3c61b113f6fa0932f482639e89d2
Sigurd Skauvik <Sigurdsk@live.no>
2014-08-20 19:02:06 +0200

    Test java functionality

--- WebContent/javatesting.js
+++ WebContent/javatesting.js
@@ -1,3 +1,24 @@
 
 // example
-alert('hi');
+// 
+var batwing = {
+	status: "Ready",
+	RescueBatmat: function() {
+		document.write("Loading his transponder... initiating launch ...");
+	}	
+}
+
+if (batwing.status === "Ready") {
+	batwing.RescueBatman();
+}
+
+var utilities = {
+	printAllMembers: function(targetObject) {
+		for (i in targetObject) {
+			document.write("<br />" + targetObject[i]);
+		}
+	}
+}
+
+utilities.printAllMembers(batwing);
+document.write("Hello");
