b11bf59bda08358d2f16c4a4591197bad3c770c3
Sigurd Skauvik <Sigurdsk@live.no>
2014-08-20 15:01:12 +0200

    Add test for javascript

--- WebContent/test.js
+++ WebContent/test.js
@@ -1,3 +1,24 @@
-/**
- * 
- */
+
+// example
+alert('hi');
+var planet = {
+	id:34,
+	name: "Sigurd Skauvik",
+	faction: [
+		factionId: 2,
+		name: "Mex",
+		notification : function() {
+		codument.write("Nex alliance.. unite!");
+		
+		}
+	}
+
+	cities: [
+		{ locationId: 15, name: "Gladius" },
+		{ locationId: 16, name: "Chalybs" },
+		{ locationId: 17, name: "Ensis" }
+	]
+};
+
+planet.faction.notification();
+document.write(planet.cities[1].name);
