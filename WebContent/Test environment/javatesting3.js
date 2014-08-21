6a6d1e253f838109237d094390f89e1895436b25
Sigurd Skauvik <Sigurdsk@live.no>
2014-08-21 21:20:30 +0200

    Complete MVA cource, Javascript development

--- WebContent/Test environment/javatesting3.js
+++ WebContent/Test environment/javatesting3.js
@@ -1,6 +1,47 @@
 // jQuery baby!
 $(function() {
  alert("This works!");
-$("#datepicker").datepicker();
- });
+  });
+  
+ /* $(function() {
+ 
+	$('#clickme').click(function() {
+	
+		$.getJSON('test1.json', function(data) {
+		
+			var items = [];
+			
+		$.each(data, function(key,val) {
+			
+			items.push('<li id="' + key + '">' + val + '</li>');
+		
+		});
+		
+		$('<ul/>', {
+			'class': 'interest-list',
+			html: items.join('')
+		}).appendTo('body');
+		
+	});
+*/
+
+$(function() {
+
+		function buildACat() {
+			var catName = "Mr. Tibbles";
+			function catFactory() {
+			
+			
+			alert(catName);
+			}
+		catFactory();
+		}
+	$('#buildcat').click(function() {
+		buildACat();
+	});
+
+});
+
+
+ 
  
