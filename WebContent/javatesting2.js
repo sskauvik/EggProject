9153750ce5a17ef6074a2e96f9246626040e8041
Sigurd Skauvik <Sigurdsk@live.no>
2014-08-21 14:36:07 +0200

    Move test envoronment to keep track of files

--- WebContent/javatesting2.js
+++ WebContent/javatesting2.js
@@ -2,3 +2,42 @@
 $(function() {
  alert("This works!");
  });
+ 
+ $(function() {
+  // By id
+  // $('#first').addClass('highlight');
+  
+  // by element
+  // $('p').addClass('highlight');
+  
+  // by class
+  // $('.chosen').addClass('highlight');
+
+  // Combinations
+  //$('#first, .chosen').addClass('highlight');
+  
+  // Contains
+  // $('li:contains("Three")').addClass('highlight');
+  
+  $('li:even').addClass('highlight');
+  
+  // Next, previous
+  // $('li:contains("Three")').prev().addClass('highlight');
+  
+  // siblings, parent
+  // $('li:contains("Three")').parent().addClass('highlight'); // selects the whole <ul>
+  
+  // $('li:nth-child(1)').addClass('highlight');
+  
+  // attributes
+  // $('p[name="mySecondPara"]').addClass('highlight');
+  
+  // $('p[name!="mySecondPara"]').addClass('highlight');
+  
+  // $('p').not('[Name]').addClass('highlight');
+  
+  // $(':header').addClass('highlight');
+  
+  $('p:empty').text('Here is some text made i jQuery :)');
+  
+  });
