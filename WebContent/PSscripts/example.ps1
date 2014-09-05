9742976d9a5322f6c3dad11baaab26886c32cdfc
Sigurd Skauvik <Sigurdsk@live.no>
2014-09-05 16:15:16 +0200

    Add help script for powershell.html

--- WebContent/PSscripts/example.ps1
+++ WebContent/PSscripts/example.ps1
@@ -1,18 +1,8 @@
-﻿# I am currently following an online cource on PowerShell. I will make this my code examples for the cource.
+﻿# DETTE SETTER OPp POWERSHEL KLART TIL BRUK 
 
-# get-help get-eventlog -showwindow
-get-eventlog -logname system -newest 5 -entrytype error
+set-executionpolicy -executionpolicy RemoteSigned
+update-help
+get-help get-help
 
-# Use get-help and look at all the syntaxes and see what you can use. 
-# Also check verb for get,set,clear etc. and find out how to do what you need to get the work done.
-# They talk alot about help and get-help
-
-# Get-process gives you a list of objects witch you can play with. 
-get-process | where handles -gt 900 | sort handles
-# If you want to see what more properties and methods 
-# in the object you can play with you can use get-member
-
-get-service -name bits | get-member
-
-# Getting only 2 properties from services
-get-service | select -Property name,status | sort -property status
+# VISER OGSÅ TIL HJELP OM HJELPEVERKTØYET I POWERSHELL "get-help" 
+# HVIS NOEN HAR INSPILL TIL MER JEG KAN LEGGE INN HER SÅ VENNLIGST SI FRA ;) 
